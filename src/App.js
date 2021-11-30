cf87047b645c1e6ff8dde45b963c719456637576
SungJoonKim <88412911+ksj0818@users.noreply.github.com>
2021-11-30 11:17:30 +0900

    복습

--- src/App.js
+++ src/App.js
@@ -2,30 +2,27 @@
 import "./App.css";
 import React, { useState } from "react";
 import Data from "./js/data";
-import Test from "./js/Test";
-import Detail from "./js/Detail";
-import Jumbotron from "./js/Jumbotron";
-import { Link, Route, Switch } from "react-router-dom";
 import {
-  Button,
-  Navbar,
   Nav,
+  Navbar,
+  Container,
   NavDropdown,
   Form,
   FormControl,
-  Container,
+  Button,
+  Card,
+  Row,
+  Col,
 } from "react-bootstrap";
+
 function App() {
   let [shoes, setShoes] = useState(Data);
+
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
         <Container fluid>
-          <Navbar.Brand>
-            <Link className="text-deco" to="/">
-              Shoes Shop
-            </Link>
-          </Navbar.Brand>
+          <Navbar.Brand href="#">Small Shop</Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav
@@ -33,13 +30,8 @@ function App() {
               style={{ maxHeight: "100px" }}
               navbarScroll
             >
-              <Nav.Link>
-                <Link className="text-deco" to="/detail/0">
-                  About
-                </Link>
-              </Nav.Link>
-              <Nav.Link>Woman</Nav.Link>
-              <Nav.Link>man</Nav.Link>
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">Link</Nav.Link>
               <NavDropdown title="Link" id="navbarScrollingDropdown">
                 <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action4">
@@ -50,6 +42,9 @@ function App() {
                   Something else here
                 </NavDropdown.Item>
               </NavDropdown>
+              <Nav.Link href="#" disabled>
+                Link
+              </Nav.Link>
             </Nav>
             <Form className="d-flex">
               <FormControl
@@ -63,23 +58,49 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-
-      {/* Switch 중복 매칭을 허용하지 않음 */}
-      <Switch>
-        {/* 메인페이지 */}
-        <Route exact path="/">
-          <Jumbotron data={shoes}></Jumbotron>
-        </Route>
-        {/* 상세페이지 */}
-        <Route path="/detail/:id">
-          <Detail data={shoes} />
-        </Route>
-        {/* 테스트페이지 */}
-        <Route path="/test">
-          <Test title="컴포넌트" />
-        </Route>
-        {/* <Route exact path="/test" component={Test}></Route> */}
-      </Switch>
+      {/* Jumbotron */}
+      <div className="jumbotron">
+        <div className="text__box">
+          <h1>Season OFF 20%</h1>
+          <p>
+            Note that depending on how they are used, badges may be confusing
+            for users of screen readers and similar assistive technologies.
+            While the styling of badges provides a visual cue as to their
+            purpose, these users will simply be presented with the content of
+            the badge. Depending on the specific situation, these badges may
+            seem like random additional words or numbers at the end of a
+            sentence, link, or button. Unless the context is clear, consider
+            including additional context with a visually hidden piece of
+            additional text.
+          </p>
+        </div>
+      </div>
+      {/* Card */}
+      <Container>
+        <Row className="center">
+          {
+            //
+            shoes.map((s) => {
+              return (
+                <Col>
+                  <Card style={{ width: "18rem" }}>
+                    <Card.Img
+                      variant="top"
+                      src="https://codingapple1.github.io/shop/shoes" + {++s.id} + ".jpg"
+                    />
+                    <Card.Body>
+                      <Card.Title>{s.title}</Card.Title>
+                      <Card.Text>{s.content}</Card.Text>
+                      <Card.Text>{s.price}</Card.Text>
+                      <Button variant="primary">Buy</Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              );
+            })
+          }
+        </Row>
+      </Container>
     </div>
   );
 }
