41845e2d67bdf6c4ba28689cab6252d757987a7f
SungJoonKim <88412911+ksj0818@users.noreply.github.com>
2021-12-08 11:41:12 +0900

    장바구니 페이지 구현

--- src/index.js
+++ src/index.js
@@ -3,20 +3,51 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+// Router
 import { BrowserRouter } from "react-router-dom";
+// Redux
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
-let store = createStore(() => {
-  return [
-    {
-      id: 0,
-      name: "멋진신발",
-      quan: 2,
-    },
-  ];
-});
+let cartUi = true;
+function reducerCart(state = cartUi, 액션) {
+  if (액션.type === "ui닫기") {
+    return false;
+  }
+  return state;
+}
+
+let 초기값 = [
+  {
+    id: 0,
+    name: "축구화",
+    quan: 4,
+  },
+  {
+    id: 1,
+    name: "야구화",
+    quan: 5,
+  },
+  {
+    id: 2,
+    name: "농구화",
+    quan: 1,
+  },
+];
+function reducer(state = 초기값, 액션) {
+  let copy = [...초기값];
+  if (액션.type === "수량증가") {
+    copy[0].quan++;
+    return copy;
+  } else if (액션.type === "수량감소") {
+    copy[0].quan--;
+    return copy;
+  } else {
+    // 변경사항이 없다면 초기값 리턴
+    return state;
+  }
+}
+let store = createStore(combineReducers({ reducer, reducerCart }));
 
 ReactDOM.render(
   <React.StrictMode>
--- src/js/Cart.js
+++ src/js/Cart.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Table } from "react-bootstrap";
 import { connect } from "react-redux";
+import "../App.css";
 
 function Cart(props) {
   return (
@@ -15,24 +16,57 @@ function Cart(props) {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>{props.state[0].id}</td>
-            <td>{props.state[0].name}</td>
-            <td>{props.state[0].quan}</td>
-            <td>Table cell</td>
-          </tr>
+          {props.state.map((s) => {
+            return (
+              <tr key={s.id}>
+                <td>{s.id}</td>
+                <td>{s.name}</td>
+                <td>{s.quan}</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      props.dispatch({ type: "수량증가" });
+                    }}
+                  >
+                    +
+                  </button>
+                  <button
+                    onClick={() => {
+                      props.dispatch({ type: "수량감소" });
+                    }}
+                  >
+                    -
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </Table>
+      {props.cartUi ? (
+        <div className="cart-ui">
+          <p>지금 구매하면 20% 할인적용!</p>
+          <button
+            className="btn btn-secondary"
+            onClick={() => {
+              props.dispatch({ type: "ui닫기" });
+            }}
+          >
+            닫기
+          </button>
+        </div>
+      ) : null}
     </div>
   );
 }
 
 function store(state) {
   return {
-    state: state,
+    state: state.reducer, // store 안에 있던 모든 데이터가 props로 등록된다.
+    cartUi: state.reducerCart,
   };
 }
-
+// connect 함수에 아까 만든 함수를 집어넣습니다. 그냥 react-redux 라이브러리 사용법입니다.
 export default connect(store)(Cart);
 
 // export default Cart;
